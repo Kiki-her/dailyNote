354c018de3f0e60a6c86b6a86a090f1f1b5529a9
kiki <k.kazaneko@gmail.com>
2023-10-01 08:11:07 +0900

    add TitleMenu

---
+++ src/TitleMenu.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function TitleMenu() {
+    // Titleが上から並ぶ
+    // クリックすると開く
+    // スワイプすると削除できる
+    // 右上にメモ追加ボタンがある
+}
