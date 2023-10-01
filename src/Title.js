edd001e27985403afecc3ad907245a27fedfdf21
kiki <k.kazaneko@gmail.com>
2023-10-01 09:47:44 +0900

    add title and buttons

--- src/Title.js
+++ src/Title.js
@@ -12,8 +12,8 @@ export default function Title() {
     ];
 
     return (
-        fakeDataObj[0] ? fakeDataObj.map((obj) => {
-            return <Text fakeDataObj={fakeDataObj} style={tailwind('font-bold')}>{obj.title}</Text>
+        fakeDataObj[0] ? fakeDataObj.map((obj, i) => {
+            return <Text key={`title${i}`} fakeDataObj={fakeDataObj} style={tailwind('font-bold text-lg border-solid border-b-4')}>{obj.title}</Text>
         }) : <Text>新しく作成する</Text>
     )
 }
--- src/TitleMenu.js
+++ src/TitleMenu.js
@@ -1,9 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Button, SafeAreaView } from 'react-native';
+import Title from './Title';
+import {useTailwind} from 'tailwind-rn';
 
 export default function TitleMenu() {
+    const tailwind = useTailwind();
     // Titleが上から並ぶ
     // クリックすると開く
     // スワイプすると削除できる
     // 右上にメモ追加ボタンがある
+    return (
+        <SafeAreaView style={tailwind('flex-1 justify-center m-0 left-0 items-stretch')}>
+         
+                <Button style={tailwind("text-xl")} onPress={() => { console.log("tap"); }} title="add button"/>
+                
+                <Title />
+                
+           
+        </SafeAreaView>
+    )
 }
